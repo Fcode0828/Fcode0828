8ead927aecd09dd88a3340237d0b779430f502fd
Fcode0828 <101631494+Fcode0828@users.noreply.github.com>
2022-04-04 20:05:16 +0800

    Add files via upload

---
+++ scanf lei/test.c
@@ -0,0 +1,55 @@
+#include"game.h"
+void menu()
+{
+	printf("************************\n");
+	printf("******1.play**0.exit****\n");
+	printf("************************\n");
+}
+void game()
+{
+	//定义棋盘
+	char mine[ROWS][COLS] = { 0 };//'0'
+	char show[ROWS][COLS] = { 0 };//'*'
+	//初始化
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	//打印棋盘
+	/*DisplayBoard(mine, ROW, COL);*/
+	DisplayBoard(show, ROW, COL);
+	//布置雷
+	SetMine(mine, ROW, COL);
+	/*DisplayBoard(mine, ROW, COL);*/
+	//扫雷 
+	FinedMine(mine, show, ROW, COL);
+	DisplayBoard(show, ROW, COL);
+
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do 
+	{
+		menu();
+		printf("请选择:>");
+			scanf("%d", &input);
+			switch (input)
+			{
+			case 1:
+				game();
+				break;
+		     case 0:
+				 printf("退出游戏");
+				break;
+			default:
+				printf("选择错误,重新选择");
+				break;
+
+			}
+	} while(input);
+}
+int main()
+{
+	test();
+	return 0;
+}
